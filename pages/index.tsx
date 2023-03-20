8e76bb6d7d9cd9f11282631626f88aa664f947f3
chad hinds <chadrickj8@gmail.com>
2023-03-20 12:20:41 -0400

    Add Paper style component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Paper } from '@mantine/core'
 
 export default function Home() {
   return (
-    <div style={{
-      position: "static",
-      height: "100vh",
-      backgroundImage: "url('https://littlevisuals.co/images/atlantic_ridge.jpg')"
-    }}></div>
+    <div
+      style={{
+        position: "static",
+        height: "100vh",
+        backgroundImage: "url('https://littlevisuals.co/images/atlantic_ridge.jpg')"
+      }}
+    >
+      <Paper withBorder p="lg" style={{ maxWidth: '400px' }}></Paper>
+    </div >
   )
 }
