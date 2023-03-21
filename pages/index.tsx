015c29eb279cb774a4a95f4f7e6b9d7b8504254b
chad hinds <chadrickj8@gmail.com>
2023-03-21 15:52:05 -0400

    Add input field

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Paper, TextInput, Button, Text, Group } from '@mantine/core'
+import { useState } from 'react'
 
 export default function Home() {
+  const [cityInput, setCityInput] = useState("")
+  console.log(cityInput)
   return (
     <div
       style={{
@@ -22,13 +25,20 @@ export default function Home() {
         }}
       >
         <Paper withBorder p="lg" style={{ maxWidth: "500px" }}>
-          <Group position='apart'>
+          <Group position="apart">
             <Text size="lg">
               Enter a city and get the weather below
             </Text>
           </Group>
-          <Group position='apart'>
-            <TextInput label="City Name" placeholder="ex: San Diego" />
+          <Group position="apart">
+            <TextInput
+              label="City Name"
+              placeholder="ex: San Diego"
+              onChange={(e) => setCityInput(e.target.value)}
+            />
+          </Group>
+          <Group position="apart">
+            <Button variant="gradient" size="md">Get Current Weather</Button>
           </Group>
         </Paper>
       </div>
