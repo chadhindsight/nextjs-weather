24b6d9cd186eafdb5ca0ff35a70f5bb87557e685
chad hinds <chadrickj8@gmail.com>
2023-03-21 15:41:21 -0400

    Add mantine components

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Paper } from '@mantine/core'
+import { Paper, TextInput, Button, Text, Group } from '@mantine/core'
 
 export default function Home() {
   return (
@@ -21,9 +21,16 @@ export default function Home() {
           transform: "translate(-50%, -50%)"
         }}
       >
-        <Paper withBorder p="lg" style={{
-          maxWidth: "500px"
-        }}></Paper>
+        <Paper withBorder p="lg" style={{ maxWidth: "500px" }}>
+          <Group position='apart'>
+            <Text size="lg">
+              Enter a city and get the weather below
+            </Text>
+          </Group>
+          <Group position='apart'>
+            <TextInput label="City Name" placeholder="ex: San Diego" />
+          </Group>
+        </Paper>
       </div>
     </div >
   )
