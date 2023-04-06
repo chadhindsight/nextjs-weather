2b8d687e43c0cc1a7d936992dec96e7435c2507b
chad hinds <chadrickj8@gmail.com>
2023-04-06 11:42:27 -0400

    optional chaining

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ export default function Home() {
               </Group>
               <Group position='left'>
                 <img
-                  src={`https://openweathermap.org/img/wn/${weahterData.weather[0].icon}@4x.png`}
+                  src={`https://openweathermap.org/img/wn/${weahterData?.weather[0]?.icon}@4x.png`}
                   width="100px"
                   height="100px"
                   alt='Matching weather icon'
